99e45b12c0e617be627fd4d3e16c4b8d0948399e
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2018-10-12 11:42:52 +0200

    Add basic package

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mix-html-builder
 A quick laravel mix extension to build up html templates from partials.
+
+## Installation
+
+```
+npm i -D mix-html-builder
+```
+
+## Usage
+
+In your `webpack.mix.js` just use `mix.buildHtml()`:
+
+```
+mix.buildHtml(
+    './src/index.html', // Your html root file
+    './out/index.html', // The html output file
+    './src/partials'    // OPTIONAL: default partial path
+);
+```
---
+++ mix-html-builder.js
@@ -0,0 +1,45 @@
+let mix = require('laravel-mix');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+class HtmlBuilder {
+    register(htmlRoot, output, partialRoot = './src/Pages') {
+        this.htmlRoot = htmlRoot;
+        this.output = output;
+        this.partialRoot = partialRoot;
+    }
+
+    dependencies() {
+        return [
+            'html-loader',
+            'posthtml',
+            'posthtml-include',
+            'posthtml-loader'
+        ];
+    }
+
+    webpackRules() {
+        return {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: 'posthtml-loader',
+                    options: {
+                        plugins: [
+                            require('posthtml-include')({ root: this.partialRoot })
+                        ]
+                    }
+                }
+            ]
+        };
+    }
+
+    webpackPlugins() {
+        return new HtmlWebpackPlugin({
+            filename: this.output,
+            template: this.htmlRoot,
+        });
+    }
+}
+
+mix.extend('buildHtml', new HtmlBuilder());
